1c773ec9d8cc66041f1f11a4f43333a8412c40d3
zoli <suvegeszoltan28@gmail.com>
2022-09-15 10:07:29 +0200

    Main.java/Points distance

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -10,11 +10,12 @@ public class Main {
         Write(points);
         int farthestPointIndex = 0;
         for (int i = 0; i < points.length; i++) {
-            if (points[farthestPointIndex].DistanceToOrigin() < points[i].DistanceToOrigin()){
+            if (points[farthestPointIndex].DistanceToOrigin() < points[i].DistanceToOrigin()) {
                 farthestPointIndex = i;
             }
         }
-        System.out.printf("A legtávolabbi pont az origótól a(z) %d pont, koordinátái: %s, origótól mért távolság: %.3f", (farthestPointIndex + 1), points[farthestPointIndex], points[farthestPointIndex].DistanceToOrigin() );
+        System.out.printf("A legtávolabbi pont az origótól a(z) %d pont, koordinátái: %s, origótól mért távolság: %.3f", (farthestPointIndex + 1), points[farthestPointIndex], points[farthestPointIndex].DistanceToOrigin());
+        System.out.printf("\nA két pont távolsága: %.3f pont az 1. pont koordinátái: %s, a 2. pont koordinátái: %s", points[0].DistanceToOtherPoint(points[1]), points[0], points[1]);
     }
 
     public static void Write(Point[] points) {
