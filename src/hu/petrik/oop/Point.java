dcb3f8953b74698cf3a32ed8887309cbfa32157e
zoli <suvegeszoltan28@gmail.com>
2022-09-15 08:43:18 +0200

    point/GetSet

--- src/hu/petrik/oop/Point.java
+++ src/hu/petrik/oop/Point.java
@@ -1,5 +1,7 @@
 package hu.petrik.oop;
 
+import java.util.Random;
+
 public class Point {
     private int x;
     private int y;
@@ -11,6 +13,27 @@ public class Point {
 
     void Point(int x, int y) {
         this.x = x;
+        this.y = y;
+    }
+
+    void Point(int n) {
+        Random rnd = new Random();
+        x = rnd.nextInt(n - (-n) + (-n));
+        y = rnd.nextInt(n - (-n) + (-n));
+    }
+
+    public int getX(){
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y) {
         this.y = y;
     }
 }
