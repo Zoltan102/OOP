f8e4aaf714716e34656bb57122260f86c6719bae
zoli <suvegeszoltan28@gmail.com>
2022-09-15 08:32:22 +0200

    point class constuctor

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -3,6 +3,6 @@ package hu.petrik.oop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/hu/petrik/oop/Point.java
@@ -0,0 +1,16 @@
+package hu.petrik.oop;
+
+public class Point {
+    private int x;
+    private int y;
+
+    void Point() {
+        x = 0;
+        y = 0;
+    }
+
+    void Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
