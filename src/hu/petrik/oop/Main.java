f38fd70c02e8aa72dc11773f3106235952962b81
zoli <suvegeszoltan28@gmail.com>
2022-09-15 08:56:24 +0200

    point/toString

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -3,6 +3,11 @@ package hu.petrik.oop;
 public class Main {
 
     public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point(0, 0);
+        Point p3 = new Point(3, 4);
+        Point p4 = new Point(5);
 
+        System.out.println(p1);
     }
 }
--- src/hu/petrik/oop/Point.java
+++ src/hu/petrik/oop/Point.java
@@ -6,17 +6,17 @@ public class Point {
     private int x;
     private int y;
 
-    void Point() {
+    public Point() {
         x = 0;
         y = 0;
     }
 
-    void Point(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    void Point(int n) {
+    public Point(int n) {
         Random rnd = new Random();
         x = rnd.nextInt(n - (-n) + (-n));
         y = rnd.nextInt(n - (-n) + (-n));
@@ -35,5 +35,12 @@ public class Point {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "[" + x + ";" + y + "]";
     }
 }
